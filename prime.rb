af779c7a70fb5ceb87300e9aafb68cd849e4eab6
natashabhuta <natasha.bhuta@gmail.com>
2018-08-18 20:29:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,5 @@
 
 def prime?(integer)
  test_divisors = (2..(integer - 1)).to_a
-  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
+  (test_divisors.any? {|divisor| integer % divisor == 0 || integer == 0 || integer == 1}) ? false : true
 end
